a4443c8ee40b043fdaa28e7661be4c37972811c3
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-25 20:42:39 +0000

    sdssd

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [username,Setusername]=useState('')
+  let [password,Setpassword]=useState('')
+  let [error,Seterror]=useState('')
+  function check(event){
+    event.preventDefault()
+    console.log(username,password)
+    if(username.length==0 || password.length==0){
+      Seterror('Both username and password are required.')
+    }
+  }
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <form onSubmit={check}>
+      <label for='name'>Username:</label>
+      <input type='text' onChange={(event)=>Setusername(event.target.value)}></input>
+      <label for='name'>Password:</label>
+      <input type='password' onChange={(event)=>Setpassword(event.target.value)}></input>
+      <div class='error'>{error}</div>
+      <button type="submit">Login</button>
+      </form>
   )
 }
 
