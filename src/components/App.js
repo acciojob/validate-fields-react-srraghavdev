ee2addabad059b6282a4507aba03e51c16a211d3
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-25 20:50:30 +0000

    sfdf

--- src/components/App.js
+++ src/components/App.js
@@ -12,14 +12,19 @@ const App = () => {
     if(username.length==0 || password.length==0){
       Seterror('Both username and password are required.')
     }
+    else{
+      Seterror('')
+    }
   }
   return (
     <form onSubmit={check}>
       <label for='name'>Username:</label>
-      <input type='text' onChange={(event)=>Setusername(event.target.value)}></input>
+      <input type='text' onChange={(event)=>{Setusername(event.target.value)
+        Seterror('')}}></input>
       <label for='name'>Password:</label>
-      <input type='password' onChange={(event)=>Setpassword(event.target.value)}></input>
-      <div id='errorMessage'>{error}</div>
+      <input type='password' onChange={(event)=>{Setpassword(event.target.value)
+      Seterror('')}}></input>
+      {error && <div id='errorMessage'>{error}</div>}
       <button type="submit">Login</button>
       </form>
   )
